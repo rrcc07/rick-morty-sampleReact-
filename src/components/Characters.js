023153dd91f90b2d24f4f24a19a4206cf3685b9e
Ronal Choque <ronalrcc07@gmail.com>
2023-02-02 18:52:38 -0400

    App rick&morty sample-react

---
+++ src/components/Characters.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export const Characters = ({characters, setCharacters}) => {
+    const resetCharacters = () => {
+        setCharacters(null)
+    }
+ 
+  return (
+    <div className='characters'>
+        <div>Characters</div>
+        <span className='back-home' onClick={resetCharacters}> come to home</span>
+        <div className='container-characters'>
+            {characters.map((character, index) => (
+                <div className='character-container' key={index} >
+                    <div>
+                        <img src={character.image} alt={character.name}></img>
+                    </div>
+                    <div>
+                        <h3> {character.name}</h3>
+                        <h6>
+                            {character.status === "Alive"  ? (
+                                <>
+                                    <span className='alive'/>
+                                    Alive
+                                </>
+                            ) : (
+                                <>
+                                    <span className='Dead'/>
+                                    Dead
+                                </>
+                            )}
+                        </h6>
+                        <p>
+                            <span  className='text-grey'>Episodies:</span>
+                            <span>{character.episode.length}</span>
+                        </p>
+                        <p>
+                            <span  className='text-grey'>Especie:</span>
+                            <span>{character.species}</span>
+                        </p>
+                    </div>
+                </div>
+            ))}
+        <span className='back-home' onClick={resetCharacters}>come to home</span>
+        </div>
+    </div>
+  )
+}
